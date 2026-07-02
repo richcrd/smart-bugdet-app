3a0f96929aeff1c45d38699f63f219e44dd93cf1
Richard Rodriguez <fam.castro99@gmail.com>
2026-07-01 21:52:22 -0600

    Fix crash when executing expo go

--- src/features/ads/consent.ts
+++ src/features/ads/consent.ts
@@ -1,8 +1,16 @@
+import Constants, { ExecutionEnvironment } from "expo-constants";
 import { Platform } from "react-native";
 import { requestTrackingPermissionsAsync } from "expo-tracking-transparency";
-import { AdsConsent, MobileAds } from "react-native-google-mobile-ads";
+
+const isExpoGo = Constants.executionEnvironment === ExecutionEnvironment.StoreClient;
 
 export async function initializeAds() {
+  if (isExpoGo) {
+    return;
+  }
+
+  const { AdsConsent, MobileAds } = require("react-native-google-mobile-ads");
+
   await AdsConsent.gatherConsent();
 
   if (Platform.OS === "ios") {
--- src/features/components/AdBanner.tsx
+++ src/features/components/AdBanner.tsx
@@ -1,7 +1,15 @@
+import Constants, { ExecutionEnvironment } from "expo-constants";
 import { StyleSheet, View } from "react-native";
-import { BannerAd, BannerAdSize, TestIds } from "react-native-google-mobile-ads";
+
+const isExpoGo = Constants.executionEnvironment === ExecutionEnvironment.StoreClient;
 
 export function AdBanner() {
+  if (isExpoGo) {
+    return null;
+  }
+
+  const { BannerAd, BannerAdSize, TestIds } = require("react-native-google-mobile-ads");
+
   return (
     <View style={styles.container}>
       <BannerAd unitId={TestIds.BANNER} size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER} />
